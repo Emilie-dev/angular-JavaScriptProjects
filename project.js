e535a2d774d8781fa5b5ebf3a69c285783322e33
Chauvin <chauvin-e@orange.fr>
2018-05-01 19:26:35 +0200

    Add AngularFire for binding data.

--- project.js
+++ project.js
@@ -12,3 +12,22 @@ Firebase location from which data are loaded.
 .service('fbRef', function(fbURL) {
 	return new Firebase(fbURL)
 })
+
+
+// An Angularfire'service for binding data from Firebase to AngularJS models.
+.service('fbAuth', function($q, $firebase, $firebaseAuth, fbRef) {
+	var auth;
+	return function () {
+	    if (auth) return $q.when(auth);
+	    var authObj = $firebaseAuth(fbRef);
+	    if (authObj.$getAuth()) {
+	    	return $q.when(auth = authObj.$getAuth());
+	    }
+	    var deferred = $q.defer();
+	    authObj.$authAnonymously().then(function(authData) {
+	    	auth = authData;
+	    	deferred.resolve(authData);
+	    });
+	    return deferred.promise;
+  	}
+})
